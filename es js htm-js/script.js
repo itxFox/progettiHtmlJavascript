46aa74476bf997d9e53c8583b10015ba6ae5d035
itxFox <145446150+itxFox@users.noreply.github.com>
2023-09-28 23:44:32 +0300

    versione html-js aggiunta

---
+++ es js htm-js/script.js
@@ -0,0 +1,30 @@
+//creo label
+const label1 = document.createElement("label");
+document.body.appendChild(label1).innerHTML = "First name:";
+
+//creo input box
+const inputBox1= document.createElement("input");
+document.body.appendChild(inputBox1);
+const br = document.createElement("br");
+document.body.appendChild(br);
+
+//creo  seconda label
+const label2 = document.createElement("label");
+document.body.appendChild(label2).innerHTML = "First name:";
+
+//creo seconda input box
+const inputBox2= document.createElement("input");
+document.body.appendChild(inputBox2);
+const br1 = document.createElement("br");
+document.body.appendChild(br1);
+
+//creo  terza label
+const label3 = document.createElement("label");
+document.body.appendChild(label3).innerHTML = "Choose your gender: M";
+
+//creo checkbox
+const checkbox1= document.createElement("input").setAttribute("type","checkbox");
+document.body.appendChild(checkbox1);
+
+
+
